2d95225a1e7c2b196f7c80547db092970d0256e8
Kirill <kirill2010.dp@gmail.com>
2022-09-18 11:23:52 +0300

    Init

--- character.py
+++ character.py
@@ -1,17 +1,13 @@
 class Character:
-    def __init__(self, name='Kirill', luck=50, hp=100, accuracy=50, armor=30, damage=10, kritdamage=20, level=11, chancekritdamage=10, maxspeed=120):
+    def __init__(self, name='Kirill', healing= 5, hp=100, accuracy=90, armor=30, damage=10, kritdamage=20, chancekritdamage=10):
         self.name = name
-        self.luck = luck
         self.hp = hp
         self.accuracy = accuracy
         self.armor = armor
         self.damage = damage
+        self.healing = healing
         self.kritdamage = kritdamage
-        self.level = level
         self.chancekritdamage = chancekritdamage
-        self.maxspeed = maxspeed
-    def attack(self, target):
-        target.take_damage(self.damage)
 
     def take_damage(self, damage):
         self.hp  -= abs(damage)
@@ -27,7 +23,5 @@ class Character:
             f' атака {self.damage}\n' \
             f' критическая атака {self.kritdamage}\n' \
             f' шанс критической атаки {self.chancekritdamage}%\n' \
-            f' максимальная скорость {self.maxspeed}\n' \
-            f' точность {self.accuracy}\n' \
-            f' удача {self.luck}\n' \
-            f' уровень {self.level}\n'
+            f' шанс попасть по врагу {self.accuracy}%\n'\
+            f' востановление здоровья {self.healing}\n'
--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 from character import Character
 import random
+a = 0
+hi = 50
+hp = 100
 player1 = Character()
 clas = input('выберите класс: ')
 if clas == 'healer':
-        hp = 100
-        while hp > 0:
-            hi = (random.randint(1, 100))
-            comand = input('в видите команду: ')
-            if comand == 'damage':
-                hp = hp - 10
-                player1.take_damage(10)
-                print(player1.stats())
-            if comand == 'healing':
-                if hp >= 100:
-                    print('maximum hp')
-                if hp <= 99:
-                    if hi <= 90:
-                        hp = hp + 5
-                        player1.take_healing(5)
-                        print(player1.stats())
-                    if hi >= 90:
-                        player1.take_damage(hp)
-                        hp = 100
-                        player1.take_healing(100)
-                        print(player1.stats())
-        print('game over')
+        while a < hi:
+            print('new game')
+            print(player1.stats())
+            while hp > 0:
+              hi = (random.randint(1, 100))
+              no = (random.randint(1, 100))
+              comand = input('в видите команду: ')
+              if comand == 'damage':
+                  if hi >= 10:
+                    if no <= 90:
+                      hp = hp - 10
+                      player1.take_damage(10)
+                      print(player1.stats())
+                  if hi <= 10:
+                   hp = hp - 20
+                   player1.take_damage(20)
+                   print(player1.stats())
+                   print('krit damage')
+                  if no >=90:
+                   print(player1.stats())
+                   print('missed')
+              if comand == 'healing':
+                  if hp >= 100:
+                      print('maximum hp')
+                  if hp <= 99:
+                      if hi <= 90:
+                          hp = hp + 5
+                          player1.take_healing(5)
+                          print(player1.stats())
+                      if hi >= 90:
+                          player1.take_damage(hp)
+                          hp = 100
+                          player1.take_healing(100)
+                          print(player1.stats())
+                          print('super healing')
+            if hp <= 0:
+                while hp<0:
+                    player1.take_healing(1)
+                    hp = hp + 1
+            if hp == 0:
+              player1.take_healing(100)
+              hp = 100
+            print('game over')
